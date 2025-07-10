a9e445cafa6a667b98f38c2631bbdf33b2583b89
sunilgyawali94@gmail.com <sunilgyawali94@gmail.com>
2025-07-10 08:16:49 +0545

    the content is no show properly

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import Navbar from "./components/Navbar"
 import Home from "./Pages/Home"
 function App() {
-  // here we communicate with javascript
+ 
 
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import "./Navbar.css";
-import logo from "../assets/logoforbilloratech.jpg";
-import { FaSearch } from "react-icons/fa";
+import Logo from "../assets/logoforbilloratech.jpg";
+import { FaSearch, FaUserAlt } from "react-icons/fa";
+import { IoCartSharp } from "react-icons/io5";
 
 function Navbar() {
   return (
-    <nav className="nav-bar">
-      <div className="logo">
-        <img src={logo} alt="MiniMart Logo" width="30" height="24" />
+
+    <>
+     <nav className="nav-bar">
+       <div className="logo">
+        <img src={Logo} alt="MiniMart Logo" width="30" height="24" />
         <span>Mini mart</span>
       </div>
 
-      <div>
+      <div className="right-secNavigation">
         <ul>
+          <li><a href="#" className="active">Home</a></li>
           <li><a href="#">Contact</a></li>
           <li><a href="#">Support</a></li>
           <li><a href="#">About</a></li>
@@ -19,11 +23,23 @@ function Navbar() {
         </ul>
       </div>
 
-      <div>
-        <FaSearch />
-        <input   type="text" className="search"   placeholder="Search the products"   />
+      <div className="search-bar">
+        <input type="text" placeholder="Search the products..." />
+        <FaSearch size={20} className="search-icon" />
       </div>
-    </nav>
+
+      <div className="right-icon">
+        <a href="#"><FaUserAlt size={20} /></a>
+      </div>
+
+      <div className="cart-set">
+        <a href="#">
+          <IoCartSharp className="cart" />
+          <span>Buy</span>
+        </a>
+      </div>
+    </nav> 
+    </>
   );
 }
 
