425f783a7cfef130b15259e11da2f41c39b43493
sunilgyawali94@gmail.com <sunilgyawali94@gmail.com>
2025-07-17 08:40:45 +0545

    this is reuseable components with useEffect and useState.

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -8,79 +8,38 @@ import Product5 from "../assets/product5.jpg";
 import Product6 from "../assets/iphone2.jpg";
 import Product7 from "../assets/product8.jpg";
 import Product8 from "../assets/Gold.jpg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Product() {
+  //usesate --> memory
+  //useEffect -->alarm
+
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setProducts(res.data);
+        console.log("You are getting data now",res.data);
+      })
+      .catch((err) => {
+        console.log("Error in fetching the json", err);
+      });
+  }, []);
   return (
     <>
       <div className="product-container reuseable-content">
-        <Customcard
-          image={Product1}
-          title={"Lenovo Yoga 7"}
-          price={"Rs. 124k"}
-          description={" Specification: Lenovo Yoga 2-in-1 16 2K Touch Laptop"}
-          button={"Buy"}
-        />
-        <Customcard
-          image={Product2}
-          title={"Xiaomi Note 13"}
-          price={">Rs. 28k"}
-          description={
-            "  Specification: Xiaomi Redmi Note 13 4G LTE (256GB + 8GB) 6.67 INCHES 108mp (midnight black)"
-          }
-          button={"add to cart"}
-        />
-
-        <Customcard
-          image={Product3}
-          title={"Ultima Boom 141 Airbuds"}
-          price={"Rs.1.9k"}
-          description={
-            " Dual Tone Matte Finish, 10m Range, 30dB ANC 45 hrs palytime ,fast charge, Available in black"
-          }
-          button={"add to cart"}
-        />
-        <Customcard
-          image={Product4}
-          title={"Ultima Magic Pro"}
-          price={"Rs. 3.5k"}
-          description={"  A premium smartwatch with a sleek zinc alloy frame"}
+        {products.map((item)=>(
+          <Customcard
+          key={item.id}
+          image={item.image}
+          title={item.title}
+          price={item.price}
+          description={item.description}
           button={"Add to cart"}
-        />
-        <Customcard
-          image={Product5}
-          title={"Nike Air Force 1"}
-          price={"Rs. 1.8k"}
-          description={
-            "  sturdy leather upper, responsive  cushioning,and versatile style"
-          }
-          button={"Add to cart"}
-        />
-
-        <Customcard
-          image={Product6}
-          title={"Iphone 16"}
-          price={"Rs. 200k"}
-          description={
-            "    Powerful A18 chip, dual 48MP cameras, 8GB RAM, and improved battery life with iOS 18 and Apple Intelligence support."
-          }
-          button={"Add to cart"}
-        />
-        <Customcard
-          image={Product7}
-          title={"Tata salt"}
-          price={"Rs.240"}
-          description={
-            "the essintaial product which plays a vitail role for a food test and used as the daily product."
-          }
-          button={"Add to cart"}
-        />
-        <Customcard
-          image={Product8}
-          title={" Gold sett"}
-          price={"Rs. 500k"}
-          description={"the gold set which make your person happy"}
-          button={"Add to cart"}
-        />
+        />))}
+       
       </div>
     </>
   );
