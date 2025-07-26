97b0c9d4e2fb39ec775b260b6b6be42421b6b0a9
sunilgyawali94@gmail.com <sunilgyawali94@gmail.com>
2025-07-26 20:12:03 +0545

    capitalization error

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,5 +1,5 @@
 import Customcard from "../components/Customcard";
-import "../Pages/Product.css";
+import "./Product.css";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
